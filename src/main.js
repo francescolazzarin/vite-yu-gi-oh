e098b3e1f0ec817928302ccb7dcd0e5972548f13
francescolazzarin <francesco.lazzarin1996@gmail.com>
2024-02-12 13:57:07 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
